f1ee44e0a800338996862d8c97b02ebcd03bbbc9
lwambon <82703590+lwambon@users.noreply.github.com>
2024-08-19 19:45:05 +0300

    Create reversestring.py

---
+++ reversestring.py
@@ -0,0 +1,8 @@
+def reverseString(s):
+    return s[::-1]
+
+# Test cases
+print(reverseString("hello"))            
+print(reverseString("JavaScript"))      
+print(reverseString("12345"))           
+print(reverseString("A man a plan a canal Panama"))  
